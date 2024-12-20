3e39208149c2c97ebd6d18489e15717aa101fdbd
GeoArchonMorax <korisawarezero@gmail.com>
2024-12-20 21:25:38 +0800

    Update geminipro.js

--- commands/geminipro.js
+++ commands/geminipro.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 module.exports = {
   name: 'geminipro',
+  category: '𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗',
   description: 'Ask a question to Gemini Pro',
   author: 'Aljur Pogoy',
   async execute(senderId, args, pageAccessToken, sendMessage) {
