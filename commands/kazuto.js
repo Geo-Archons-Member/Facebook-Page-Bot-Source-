637db34624c2ff2a58712c045709d58bb92dc454
GeoArchonMorax <korisawarezero@gmail.com>
2024-12-20 21:26:50 +0800

    Update kazuto.js

--- commands/kazuto.js
+++ commands/kazuto.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 module.exports = {
   name: 'kazuto',
+  category: '𝙴𝚍𝚞𝚌𝚊𝚝𝚒𝚘𝚗',
   admin: false,// you can change it, like author: ""
   execute: async (senderId, args, pageAccessToken, sendMessage) => {
     const question = args.join(' ');
